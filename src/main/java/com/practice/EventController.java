e3d581cdabfc952e1481ab41e5cdfcea4a1ab58b
de l'Hamaide <delhamaide.florent@gmail.com>
2019-10-02 18:04:03 +0200

    add init front

--- src/main/java/com/practice/EventController.java
+++ src/main/java/com/practice/EventController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/event")
+@RequestMapping("/api/events")
 public class EventController {
 
     private final EventService eventService;
